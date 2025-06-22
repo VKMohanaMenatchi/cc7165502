91b1518fb0790bfe75fb0b7c10ab848290a5273e
MohanaMenatchiVK <727723eucb030@skcet.ac.in>
2025-06-22 08:47:18 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const Event = require("./models/Event"); // Import Mongoose Event model
+const Event = require("./model/Event"); 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
